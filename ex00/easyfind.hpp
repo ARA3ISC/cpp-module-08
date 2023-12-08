49bdd1fdd683c51d71761d9e1ab1b605f0105d5f
MacBook Air <ziz@azizs-MacBook-Air.local>
2023-12-08 14:44:12 +0100

    before puuush

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -7,13 +7,8 @@
 # include <algorithm>
 
 template <typename T>
-void easyfind(T container, int toFind)
+typename T::iterator easyfind(T& container, int toFind)
 {
-	typename T::iterator it = std::find(container.begin(), container.end(), toFind);
-
-	if (it != container.end())
-		std::cout << "Number found at index " << std::distance(container.begin(), it) << std::endl;
-	else
-		throw std::runtime_error("Number not found");
+	return std::find(container.begin(), container.end(), toFind);
 }
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -15,25 +15,44 @@
 int main()
 {
 	try
-	{ // ? vector version
+	{
+		// ? vector version
 		std::vector<int> v;
 		for (int i = 4; i < 6; i++)
 			v.push_back(i);
-		easyfind(v, 5);
+
+		std::vector<int>::iterator it = easyfind(v, 5);
+		if (it != v.end())
+			std::cout << "found at index " << std::distance(v.begin(), it) << std::endl;
+		else
+			std::cout << "number doesn't exist" << std::endl;
+
+
 
 		// ? list version
 		// std::list<int> l;
 		// for (int i = 0; i < 47; i++)
 		// 	l.push_back(i);
-		// easyfind(l, 99);
+		// std::list<int>::iterator it = easyfind(l, 11);
 
-		// ? deque version
+		// if (it != l.end())
+		// 	std::cout << "found at index " << std::distance(l.begin(), it) << std::endl;
+		// else
+		// 	std::cout << "number doesn't exist" << std::endl;
+
+
+		// // ? deque version
 		// std::deque<int> q;
 		// q.push_back(34);
 		// q.push_back(1);
 		// q.push_back(45);
 		// q.push_back(98);
-		// easyfind(q, 98);
+		// std::deque<int>::iterator it = easyfind(q, 980);
+
+		// if (it != q.end())
+		// 	std::cout << "found at index " << std::distance(q.begin(), it) << std::endl;
+		// else
+		// 	std::cout << "number doesn't exist" << std::endl;
 	}
 	catch (std::exception& e)
 	{
